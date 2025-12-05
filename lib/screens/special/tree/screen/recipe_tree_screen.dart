33f7f5e271183205c5ff77e27f6c97f87ee78555
jun0606 <jwj0606@nate.com>
2025-12-05 14:34:07 +0900

    수정: recipe_tree_screen에 AppLocalizations import 추가

--- lib/screens/special/tree/screen/recipe_tree_screen.dart
+++ lib/screens/special/tree/screen/recipe_tree_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../../../../l10n/app_localizations.dart';
 import '../../../../models/recipe.dart';
 import '../../../../providers/recipe_provider.dart';
 import '../../../../services/recipe_derivation_service.dart';
